8fa84226ec516579e71eef8eab8350b1cacb97b0
Krister Viirsaar <krister.viirsaar@gmail.com>
2019-01-23 18:45:45 +0200

    Add database class and library

---
+++ src/sample/Andmebaas.java
@@ -0,0 +1,51 @@
+import java.sql.*;
+
+public class Andmebaas {
+    Connection conn = null;
+
+    // Constructor, ehk meetod mis käivitub kohe objekti välja kutsumisel
+    public Andmebaas() {
+        looYhendus();
+        looTabel();
+    }
+
+    // Et andmebaasi kasutada peame sellega esiteks ühenduse looma
+    private void looYhendus() {
+        try {
+            Class.forName("org.sqlite.JDBC");                          // Lae draiver sqlite.jar failist
+            conn = DriverManager.getConnection("jdbc:sqlite:test.db"); // loo ühendus andmebaasi failiga
+        } catch ( Exception e ) {                                      // püüa kinni võimalikud errorid
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        System.out.println("Opened database successfully");            // lihtsalt meie enda jaoks teade
+    }
+
+    // Et andmebaasist kasu oleks, loome uue tabeli. See on nagu uus 'sheet' excelis.
+    public void looTabel() {
+    }
+
+    // Andmebaasi muudatused ei tagasta väärtusi (erinevalt
+    // päringutest) ja on lihtne eraldi meetodi tuua.
+    private void teostaAndmebaasiMuudatus(String sql) {
+        try {
+            // Statement objekt on vajalik, et SQL_Login käsku käivitada
+            Statement stat = conn.createStatement();
+            stat.executeUpdate(sql);
+            stat.close(); // Statement tuleb samuti kinni panna nagu ka Connection.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Kui programmis avad ainult ühendusi ja ühtegi ei sulge siis see kulutab arvuti (serveri) ressursse.
+    // Varsti võib masin kokku joosta.
+    public void sulgeYhendus() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ühendus suletud");
+    }
+
+}
